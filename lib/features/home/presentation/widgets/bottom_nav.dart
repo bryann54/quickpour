358faea3f4d942d932e28299bd12712e100c37ad
bryann54 <alfredbryann545@gmail.com>
2024-12-11 09:01:56 +0300

    search ui

--- lib/features/home/presentation/widgets/bottom_nav.dart
+++ lib/features/home/presentation/widgets/bottom_nav.dart
@@ -7,6 +7,7 @@ import 'package:chupachap/features/favorites/presentation/bloc/favorites_state.d
 import 'package:chupachap/features/favorites/presentation/pages/favorites_screen.dart';
 import 'package:chupachap/features/home/presentation/pages/home_screen.dart';
 import 'package:chupachap/features/orders/presentation/pages/orders_screen.dart';
+import 'package:chupachap/features/search/presentation/pages/search_page.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -27,9 +28,11 @@ class _BottomNavState extends State<BottomNav> {
   // List of screens corresponding to navigation items
   final List<Widget> _screens = [
     const HomeScreen(),
+     const SearchPage(),
     const FavoritesScreen(),
     const CartPage(),
      const OrdersScreen(),
+    
   ];
 
   @override
@@ -42,6 +45,8 @@ class _BottomNavState extends State<BottomNav> {
         height: 60.0,
         items: [
           const FaIcon(FontAwesomeIcons.houseChimney,
+              size: 25, color: Colors.white),
+                        const FaIcon(FontAwesomeIcons.searchengin,
               size: 25, color: Colors.white),
 
       // Favorites with badge
