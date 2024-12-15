6d74597d03e14df2a52b6603c0e751aa2a8fcb23
bryann54 <alfredbryann545@gmail.com>
2024-12-15 08:58:09 +0300

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
-import 'package:chupachap/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'app.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Load the .env file
   await dotenv.load(fileName: ".env");
+
+  // Initialize Firebase with the generated options
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const App());
 }
