4aa807cbc15ace4cf39bd8159b5c6c222d03a7c9
bryann54 <alfredbryann545@gmail.com>
2025-01-10 15:58:57 +0300

    added android maps

--- android/app/src/main/kotlin/com/example/chupachap/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chupachap/MainActivity.kt
@@ -1,5 +1,26 @@
 package com.example.chupachap
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.yourapp.maps"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "setApiKey" -> {
+                    // The API key is now handled through AndroidManifest.xml and strings.xml
+                    // Just return success since we don't need to do anything here
+                    result.success(null)
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+}
