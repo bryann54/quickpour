51bf02db01d01623754166aecad2ea1924b9dab3
bryann54 <alfredbryann545@gmail.com>
2025-03-02 10:34:21 +0300

    Add share_plus and printing dependencies; update order_details.dart to include ShareButton

--- lib/features/orders/presentation/pages/order_details.dart
+++ lib/features/orders/presentation/pages/order_details.dart
@@ -3,6 +3,7 @@ import 'package:chupachap/features/orders/presentation/widgets/customer_details_
 import 'package:chupachap/features/orders/presentation/widgets/order_header_widget.dart';
 import 'package:chupachap/features/orders/presentation/widgets/order_item_section.dart';
 import 'package:chupachap/features/orders/presentation/widgets/payment_section_widget.dart';
+import 'package:chupachap/features/orders/presentation/widgets/widgets/share_button.dart';
 import 'package:flutter/material.dart';
 import 'package:chupachap/core/utils/colors.dart';
 import 'package:chupachap/features/orders/data/models/completed_order_model.dart';
@@ -21,12 +22,18 @@ class OrderDetails extends StatelessWidget {
       backgroundColor: isDark ? AppColors.backgroundDark : AppColors.background,
       // appBar: CustomAppBar(showCart: false),
       appBar: AppBar(
-        title: Text(
-          'Order Details',
-          style: theme.textTheme.headlineSmall?.copyWith(
-            fontWeight: FontWeight.bold,
-            color: isDark ? AppColors.textPrimaryDark : AppColors.textPrimary,
-          ),
+        title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Order Details',
+              style: theme.textTheme.headlineSmall?.copyWith(
+                fontWeight: FontWeight.bold,
+                color: isDark ? AppColors.textPrimaryDark : AppColors.textPrimary,
+              ),
+            ),
+            ShareButton(order: order,),
+       
+          ],
         ),
       ),
       body: SingleChildScrollView(
